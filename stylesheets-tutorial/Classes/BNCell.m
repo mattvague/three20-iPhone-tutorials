33e74f2203fb60b6004c62ba2e8b4d08be62a1dd
Matt Vague <matt@mattvague.com>
2009-06-24 18:00:22 -0700

    Made stylsheets tutorial into a folder

---
+++ stylesheets-tutorial/Classes/BNCell.m
@@ -0,0 +1,43 @@
+////////////////////////////////////////
+//// BNGroup Cells for Group View
+////////////////////////////////////////
+//
+//  Created by Matt Vague on 10/04/09.
+//
+
+#import "BNCell.h"
+#import "BNDefaultStylesheet.h"
+
+///////////////////////////////////////////////////////////////////////////////////////////////////
+
+///////////////////////////////////////
+//////   BNBrowseListFieldCell     ////
+///////////////////////////////////////
+
+@implementation BNSubtextTableFieldCell
+
+///////////////////////////////////////////////////////////////////////////////////////////////////
+
+- (void)setObject:(id)object {
+	if (_field != object) {
+		[super setObject:object];
+		
+		TTSubtextTableField* field = object;
+		_label.text = field.text;
+		_label.font = TTSTYLEVAR(myFirstFont);
+		_label.textColor = TTSTYLEVAR(myFirstColor);
+		_label.adjustsFontSizeToFitWidth = YES;
+		
+		_subtextLabel.text = field.subtext;
+		_subtextLabel.font = TTSTYLEVAR(mySecondFont);
+		_subtextLabel.textColor = TTSTYLEVAR(mySecondColor);
+	}  
+}
+
+
+@end
+
+
+///////////////////////////////////////////////////////////////////////////////////////////////////
+
+
---
+++ stylesheets-tutorial/Classes/BasicNavigationAppAppDelegate.h
@@ -0,0 +1,11 @@
+@interface BasicNavigationAppAppDelegate : NSObject <UIApplicationDelegate> {
+    
+    UIWindow *window;
+    UINavigationController *navigationController;
+}
+
+@property (nonatomic, retain) IBOutlet UIWindow *window;
+@property (nonatomic, retain) IBOutlet UINavigationController *navigationController;
+
+@end
+
